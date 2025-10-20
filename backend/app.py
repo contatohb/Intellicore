f034f5630e11ecc85406d8eaa4908067398b7124
contatohb <contato@hb-advisory.com.br>
2025-10-20 10:39:37 -0300

    Register ingest API blueprint in app

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify
 import os
 from routes.test_db import test_db
 from routes.log_event import log_api
+from routes.ingest import ingest_api
 from logger import log_event
 
 app = Flask(__name__)
 app.register_blueprint(test_db)
 app.register_blueprint(log_api)
+app.register_blueprint(ingest_api)
 
 @app.route("/")
 def home():
